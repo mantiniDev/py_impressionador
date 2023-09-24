2c2de3623205381afbeb349079e953db3958487a
mantiniDev <pmantinidev@gmail.com>
2023-09-23 23:08:19 -0300

    23.09.23 - 23:0

--- integração _api/api_moeda.py
+++ integração _api/api_moeda.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
-cotacoes_btc = requests.get('https://economia.awesomeapi.com.br/BTC-BRL/200?start_date=20200101&end_date=20201031')
+cotacoes_btc = requests.get('https://economia.awesomeapi.com.br/BTC-BRL/200?start_date=20200101&end_date=20201231')
 
 btc_dic = cotacoes_btc.json()
 print(btc_dic)
 lista_cotacoes_btc = [item['bid'] for item in btc_dic]
 print(lista_cotacoes_btc)
+print(len(lista_cotacoes_btc))
