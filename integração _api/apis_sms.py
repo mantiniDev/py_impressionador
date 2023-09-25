1964f5aa7a0bca0b64674cda0dd17a2e14205553
mantiniDev <pmantinidev@gmail.com>
2023-09-25 12:35:49 -0300

    25.09.23 - 12:35 -  api_sms

--- integração _api/apis_sms.py
+++ integração _api/apis_sms.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC97043c4b8ce06f6658f193e6a6015e2e'
-auth_token = '12e606d0e5192e8af20f729889ff0485'
+account_sid = 'token'
+auth_token = 'token'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
